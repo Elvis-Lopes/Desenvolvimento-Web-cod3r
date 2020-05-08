92df063f4be8fe0dad52999d6e060c239e66ab0d
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-07 21:59:13 -0300

    aula sobre tipagem

---
+++ exercicios-js/fundamentos/tipagem fraca.js
@@ -0,0 +1,8 @@
+let qualquer = "legal"
+console.log(qualquer)
+// o typeof retorna o tipo da variavel
+console.log(typeof qualquer)
+
+qualquer = 3.124134
+console.log(qualquer)
+console.log(typeof qualquer)
