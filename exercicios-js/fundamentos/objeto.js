9cc568e0584e4894568db416a3b2b8ebc1edbd1a
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-13 14:04:42 -0300

    aulas sobre array e objetos

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,19 @@
+/*  
+    para criar um objeto a variavel irá receber um "{}" e
+    dentro dele vai estar os atributos do projeto
+*/
+
+// instanciado  um objeto vazio
+const prod1 ={}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 999.99
+console.log(prod1)
+prod1['Desconto legal: '] = 0.40
+console.log(prod1)
+
+const prod2 ={
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
