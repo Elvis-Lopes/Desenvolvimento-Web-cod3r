bb78342778d220e97714b128618d8ab64f588f91
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-19 10:46:45 -0300

    add das aulas sobre conceitos de funções e vars

---
+++ exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,16 @@
+// função sem retorno
+function imprimirSoma(a , b=0 )
+{
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+
+// função com retorno
+function soma(a, b = 0)
+{
+    return a + b
+}
+
+console.log(soma(2, 3))
