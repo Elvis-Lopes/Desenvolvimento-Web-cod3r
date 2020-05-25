c0afed451cdce8ccf3fca5c89acabfa2db16ae85
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-25 16:45:12 -0300

    aula sobre operadores logicos

---
+++ exercicios-js/fundamentos/aritimeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+console.log(soma)
+
+const subtracao = a - b - c -d 
+const mulitplicacao = a * b
+const divisao = d / a
+const modulo = a %2
+console.log(subtracao)
+console.log(divisao)
+console.log(mulitplicacao)
+console.log(modulo)
