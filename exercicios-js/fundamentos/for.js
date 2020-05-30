796a25c6f32d523f2ad4a41c295e30d1c1670c26
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-30 11:35:03 -0300

    add da aula de For-In

--- exercicios-js/fundamentos/for.js
+++ exercicios-js/fundamentos/for.js
@@ -1,6 +1,10 @@
-let contador = 1
-
-for (i = contador; i <= 10; i++)
+for (let i = 1; i <= 10; i++)
 {
     console.log(`contador = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for (let i = 0; i < notas.length; i++)
+{
+    console.log(`nota = ${notas[i]}`)
 }
