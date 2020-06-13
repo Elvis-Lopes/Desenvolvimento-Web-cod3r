8de50875e8febc8c5950aa843a4c4171fb5defd7
Elvis Lopes <elvislopes1996@hotmail.com>
2020-06-13 14:43:19 -0300

    aula de função construtora

--- função/funcaoCallBack2.js
+++ função/funcaoCallBack2.js
@@ -1,14 +1,24 @@
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
 
-function notaMaior(nota)
+// Sem callback
+let notasBaixas1 = []
+for (let i in notas)
 {
-    if (nota >= 7)
+    if(notas[i] < 7)
     {
-        console.log(nota)
+        notasBaixas1.push(notas[i])
     }
 }
-for ( let i in notas) 
-{
-    if(notas[i])
-}
-notas.forEach(notaMaior)
+
+console.log(notasBaixas1)
+
+// Com callback
+const notasBaixas2 = notas.filter(function(nota){
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+const notasMenorQue7 = nota => nota < 7
+const notasBaixas3 = notasBaixas1.filter(notas => notas < 7)
+console.log(notasBaixas3)
