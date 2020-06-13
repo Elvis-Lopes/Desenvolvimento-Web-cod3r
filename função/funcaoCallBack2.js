401a1eed10d15f9b7d5f1087b90fd0f7f9ac9c2d
Elvis Lopes <elvislopes1996@hotmail.com>
2020-06-13 10:22:38 -0300

    aula de função callback

---
+++ função/funcaoCallBack2.js
@@ -0,0 +1,14 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+function notaMaior(nota)
+{
+    if (nota >= 7)
+    {
+        console.log(nota)
+    }
+}
+for ( let i in notas) 
+{
+    if(notas[i])
+}
+notas.forEach(notaMaior)
