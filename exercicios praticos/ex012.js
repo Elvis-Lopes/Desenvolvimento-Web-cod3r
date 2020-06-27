2bc5f6f63ae067574cf71eca21b4419e8092862e
Elvis Lopes <elvislopes1996@hotmail.com>
2020-06-27 18:39:41 -0300

    add ex 14 ao 15

--- exercicios praticos/ex012.js
+++ exercicios praticos/ex012.js
@@ -3,11 +3,15 @@
 function fatorial(numero){
     let resultado = 1
 
-    for(numero; numero >=1; numero--){
-        resultado *= numero
-    }
+    if(numero == 0){
+        console.log('Resultado:')
+    }else{
+        for(numero; numero >=1; numero--){
+            resultado *= numero
+        }
 
-    console.log(`Resultado ${resultado}`)
+        console.log(`Resultado: ${resultado}`)
+    }
 }
 
 fatorial(3)
