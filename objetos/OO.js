286dfc4828bf6ada1ac5e5a5e5a711762ff16146
Elvis Lopes <elvislopes1996@hotmail.com>
2020-07-14 21:11:35 -0300

    add das aulas sobre objetos

---
+++ objetos/OO.js
@@ -0,0 +1,25 @@
+// código não executavel
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+/*
+ * Principios da OO
+ * 1- Abstração
+ * 2- Encapsulamento
+ * 3- Objeto
+ * 4- Polimorfismo
+ */
