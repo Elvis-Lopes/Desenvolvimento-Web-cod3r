345ae5947ffd260c2edd44fe94a6b698da1daca1
Elvis Lopes <elvislopes1996@hotmail.com>
2020-08-09 08:21:14 -0300

    add aula 1 de foreach

---
+++ objetos/foreach.js
@@ -0,0 +1,17 @@
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+const aprovados2 = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1} ${nome}`)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
+
+aprovados2.forEach(function(nome, indice, array){
+    console.log(`${indice + 1} ${nome}`)
+    console.log(array)
+})
