6d415677f17e42e235cec18987b04d046410a063
Elvis Lopes <elvislopes1996@hotmail.com>
2020-09-20 16:54:13 -0300

    add das aulas de modulo e outras funções de js

---
+++ node/moduloCliente.js
@@ -0,0 +1,9 @@
+const moduloA = require('./modulaA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.bemVindo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
---
+++ objetos/imperativoVsDeclaritivo.js
@@ -0,0 +1,16 @@
+const aluno = [
+    {nome: 'João', nota: 7.9},
+    {nome:'Maria', nota: 9.2} 
+]
+//imperativo
+let total1 = 0
+for(let i = 0; i < aluno.length; i++){
+    total1 += aluno[i].nota
+}
+console.log(total1/aluno.length)
+
+//declarativo
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+const total2 = aluno.map(getNota).reduce(soma)
+console.log(total2/aluno.length)
